4cb72e7126e0d96a6e2a1eb9aab302d1d8536631
Ilfat <78148851+Il777-hash@users.noreply.github.com>
2022-08-01 22:12:12 +0500

    Добавьте файлы проекта.

---
+++ ASP2/Controllers/CalcController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ASP2.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Add(int x, int y)
+        {
+            int result = x + y;
+            return View("Index", result.ToString());
+        }
+        public IActionResult Mul(int x, int y)
+        {
+            int result = x * y;
+            return View("Index", result.ToString());
+        }
+        public IActionResult Div(int x, int y)
+        {
+            string result;
+			if (y == 0)
+			{
+                result = "Деление на ноль";
+			}
+            else result = (Convert.ToDouble(x) / y).ToString();
+            return View("Index", result);
+        }
+        public IActionResult Sub(int x, int y)
+        {
+            int result = x - y;
+            return View("Index", result.ToString());
+        }
+    }
+}
